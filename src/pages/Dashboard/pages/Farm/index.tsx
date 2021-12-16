1b7bc9ef5b077741e5a7fd75857e5b6deb9d8bcd
Hikaru Chang <i@rua.moe>
2021-12-16 17:30:52 +0800

    fix: farm view

--- src/pages/Dashboard/pages/Farm/index.tsx
+++ src/pages/Dashboard/pages/Farm/index.tsx
@@ -13,7 +13,7 @@ const ICON_USDT = '/images/crypto/usdt-circle.svg';
 const ICON_USDC = '/images/crypto/usdc-circle.svg';
 
 const Farm: React.FC = () => {
-    const [collapse, setCollapse] = useState<boolean>(false);
+    const [collapse, setCollapse] = useState<Record<number, boolean>>({});
     const [loading, setLoading] = useState<boolean>(false);
 
     const {
@@ -152,17 +152,127 @@ const Farm: React.FC = () => {
                                                         type="link"
                                                         icon={
                                                             <DownOutlined
-                                                                rotate={!collapse ? 0 : -180}
+                                                                rotate={!collapse[0] ? 0 : -180}
                                                                 className={style.expandButtonIcon}
                                                             />
                                                         }
                                                         onClick={() => {
-                                                            setCollapse(!collapse);
+                                                            setCollapse({ ...collapse, 0: !collapse[0] });
                                                         }}
                                                     />
                                                 </div>
                                             </div>
-                                            <Rows collapse={collapse} />
+                                            <Rows collapse={collapse[0]} />
+                                        </div>
+                                    </Badge.Ribbon>
+                                    <Badge.Ribbon
+                                        text={
+                                            <>
+                                                <Space>
+                                                    <FireOutlined />
+                                                    Double reward
+                                                </Space>
+                                            </>
+                                        }
+                                    >
+                                        <div className={style.stakeItem}>
+                                            <div className={style.stakeMain}>
+                                                <div className={style.tokenPair}>
+                                                    <div className={style.tokenIcons}>
+                                                        <Image
+                                                            src={ICON_ETH}
+                                                            preview={false}
+                                                            className={style.icon}
+                                                        />
+                                                        <Image
+                                                            src={ICON_AD3}
+                                                            preview={false}
+                                                            className={style.icon}
+                                                        />
+                                                    </div>
+                                                    <div className={style.tokenNameAndRate}>
+                                                        <div className={style.tokenName}>
+                                                            ETH/AD3
+                                                        </div>
+                                                        <div className={style.tokenRate}>
+                                                            <Space>
+                                                                Free Rate
+                                                                <strong>0.05%</strong>
+                                                            </Space>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div className={style.tokenAPY}>
+                                                    <div className={style.title}>
+                                                        APY(1y)
+                                                    </div>
+                                                    <div className={style.value}>
+                                                        2.05%
+                                                        <Tooltip
+                                                            placement="bottom"
+                                                            title={'The APR value is calculated based on the current data, which changes as the user deposition changes.'}
+                                                        >
+                                                            <InfoCircleOutlined className={style.tipButton} />
+                                                        </Tooltip>
+                                                    </div>
+                                                </div>
+                                                <div className={style.tokenLiquidity}>
+                                                    <div className={style.title}>
+                                                        Liquidity
+                                                    </div>
+                                                    <div className={style.value}>
+                                                        $300,474.57
+                                                        <Tooltip
+                                                            placement="bottom"
+                                                            title={'The APR value is calculated based on the current data, which changes as the user deposition changes.'}
+                                                        >
+                                                            <InfoCircleOutlined className={style.tipButton} />
+                                                        </Tooltip>
+                                                    </div>
+                                                </div>
+                                                <div className={style.tokenRewardRange}>
+                                                    <div className={style.title}>
+                                                        Reward Range
+                                                    </div>
+                                                    <div className={style.value}>
+                                                        0.95-1.05
+                                                        <Tooltip
+                                                            placement="bottom"
+                                                            title={'The APR value is calculated based on the current data, which changes as the user deposition changes.'}
+                                                        >
+                                                            <InfoCircleOutlined className={style.tipButton} />
+                                                        </Tooltip>
+                                                    </div>
+                                                </div>
+                                                <div className={style.tokenRewards}>
+                                                    <div className={style.title}>
+                                                        Rewards
+                                                    </div>
+                                                    <div className={style.value}>
+                                                        <Image
+                                                            src={'/images/logo-round-core.svg'}
+                                                            preview={false}
+                                                            className={style.icon}
+                                                        />
+                                                        10 AD3/block
+                                                    </div>
+                                                </div>
+                                                <div className={style.expandButton}>
+                                                    <Button
+                                                        type="link"
+                                                        icon={
+                                                            <DownOutlined
+                                                                rotate={!collapse[1] ? 0 : -180}
+                                                                className={style.expandButtonIcon}
+                                                            />
+                                                        }
+                                                        onClick={() => {
+                                                            setCollapse({ ...collapse, 1: !collapse[1] });
+                                                        }}
+                                                    />
+                                                </div>
+                                            </div>
+                                            <Rows collapse={collapse[1]} />
                                         </div>
                                     </Badge.Ribbon>
                                 </div>
