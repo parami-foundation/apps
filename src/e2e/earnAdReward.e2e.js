8d93b8b56c3d65b358852d4f5818354a46a632fc
RightSun <right.sun@outlook.com>
2022-01-07 21:00:25 +0800

    add a test account in test

--- src/e2e/earnAdReward.e2e.js
+++ src/e2e/earnAdReward.e2e.js
@@ -3,7 +3,17 @@ import config from "../config/config";
 const KOL_URL = 'https://wallet.parami.io/did:ad3:2BNi8qXrGFBLWLJcGyaNg9yyGmWE?referrer=0x549e237e4154630d1467a3250b7ad118f216ea27';
 const HOME_URL = 'https://wallet.parami.io/';
 
-
+beforeAll(async () => {
+    await page.goto(HOME_URL);
+    await page.evaluate(() => {
+        localStorage.setItem('controllerKeystore', 'eaeb15bc3ad07183e668bdaa7cce8310ab53cab834b85be79d06cdf7db4f4c23ea722d2d02fa049f402b31e5939b0671ea8f32fd1ab9948bceac63462dbea5a65c8a2febd253676765d59941bb7650bc');
+        localStorage.setItem('controllerUserAddress', '5GpyXikKcJBdT2sKV2AYQS9duH4HmXAs7Xhf1scs22vYFEBm');
+        localStorage.setItem('stamp', '7191d5');
+        localStorage.setItem('magicUserAddress', '5C5vhYWyBT4EYaaNZNiaQsjxeo6phtmuxL7V9BRQh8s7si6S');
+        localStorage.setItem('did', '0xc6636a8106809919bb9252881259e0b0c6f6d58c');
+        localStorage.setItem('stashUserAddress', '5EYCAe5ijQkVKBy52wmC34BKLy4Sc8Fey42mry1a7DkHzEPG');
+    });
+});
 describe('get Ad reward', () => {
     it('check create button', async () => {
         await page.goto(HOME_URL);
