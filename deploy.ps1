d3b52f9851145dbc83f7f03d4a23c9a092585732
right.sun <szz@joymoe.com>
2022-05-13 22:41:13 +0800

    update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -6,13 +6,10 @@ cd dist
 cp index.html 404.html
 
 git init .
-
-git branch -M gh-pages
-git remote add origin git@github.com:parami-protocol/apps.git
-
 git add -f .
 git commit -m "Deploy"
-
+git branch -M gh-pages
+git remote add origin git@github.com:parami-protocol/apps.git
 git push -f origin gh-pages
 cd ..
 }
