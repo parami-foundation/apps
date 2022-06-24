b495a2f4cd3547d504c1213005d856ea51c5ddf3
Kai Kang <kaikang@KaideMacBook-Air.local>
2022-06-24 12:35:30 +0800

    MOD: fix deploy.sh syntax error

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,18 @@ if [ $? -eq 0 ]; then
     git init .
     git branch -M gh-pages
     if [ "$1" = "staging" ]; then
+        echo "deploying to apps-staging"
         git remote add origin git@github.com:parami-protocol/apps-staging.git
     else
+        echo "deploying to apps"
         git remote add origin git@github.com:parami-protocol/apps.git
+    fi
     git add -f .
     git commit -m "Deploy"
     git push -f origin gh-pages
     cd ..
+    echo "Deploy success"
 else
-     echo "build failed"
+    echo "build failed"
 fi
 
