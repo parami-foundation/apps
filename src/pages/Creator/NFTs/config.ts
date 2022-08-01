702a080fb189bc642a9557e982dd11b0a5751137
Kai Kang <kaikang@KaideMacBook-Air.local>
2022-08-01 17:30:58 +0800

    ADD: new Registry contract address on rinkeby

--- src/pages/Creator/NFTs/config.ts
+++ src/pages/Creator/NFTs/config.ts
@@ -1,7 +1,7 @@
 
 export const registryAddresses = {
-  1: '0x0833624A59E827203965bC1eb866eEe1B2D2E49B', // main
-  4: '0xc0981bF737F5D4e1AF738133b787693B17aa7DCE' // rinkeby
+  1: '0x0833624A59E827203965bC1eb866eEe1B2D2E49B', // todo: deploy mainnet
+  4: '0xbf9009DB1E3934F48a5Ce038cC3aca8a20e3cD8E' // rinkeby
 }
 
 export const HCollectionAddress = {
