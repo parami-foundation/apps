9c3ed8c703228364d95c785e884a253bd67ed56c
kaikang.eth <cctv2206@outlook.com>
2022-08-22 12:42:25 +0800

    MOD: discord login no new window

--- src/components/Discord/DiscordLoginButton.tsx
+++ src/components/Discord/DiscordLoginButton.tsx
@@ -14,7 +14,8 @@ const DiscordLoginButton: React.FC<{
   };
 
   const onClick = () => {
-    window.open(`${config.airdropService.discord.oauthEndpoint}?response_type=token&client_id=${clientId}&scope=identify&redirect_uri=${RedirectUri}&state=airdrop`, 'discordLogin', 'height=700,width=500');
+    window.location.href = `${config.airdropService.discord.oauthEndpoint}?response_type=token&client_id=${clientId}&scope=identify&redirect_uri=${RedirectUri}&state=airdrop`;
+    // window.open(`${config.airdropService.discord.oauthEndpoint}?response_type=token&client_id=${clientId}&scope=identify&redirect_uri=${RedirectUri}&state=airdrop`, 'discordLogin', 'height=700,width=500');
   }
 
   return (
