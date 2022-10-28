b3acab856356309bceaee28e007467cab7db177a
kaikang.eth <cctv2206@outlook.com>
2022-10-28 12:21:50 +0800

    MOD: update ad preview ui

--- src/components/Advertisement/AdvertisementPreview/AdvertisementPreview.tsx
+++ src/components/Advertisement/AdvertisementPreview/AdvertisementPreview.tsx
@@ -1,11 +1,13 @@
 import { Tooltip } from 'antd';
 import React, { useState } from 'react';
 import './AdvertisementPreview.less';
+import bubble from './bubble.svg';
 
 const AdvertisementPreview: React.FC<{
 	ad: any;
 }> = ({ ad }) => {
 	const [claimText, setClaimText] = useState<string>('Not interested, claim it now');
+	const [claimed, setClaimed] = useState<boolean>(false);
 
 	const tags = (ad?.instructions ?? []).map((instruction: any) => instruction.tag).filter(Boolean);
 
@@ -15,7 +17,7 @@ const AdvertisementPreview: React.FC<{
 	return (
 		<>
 			<div className='advertisementContainer'>
-				{<>
+				<>
 					<div className='ownerInfo'>
 						<span>📢 This hNFT is reserved.</span>
 						<a className='claimLink' href={``} target='_blank'>I am the owner</a>
@@ -35,29 +37,31 @@ const AdvertisementPreview: React.FC<{
 									{sponsorName}
 								</span>
 							</>}
-							is sponsoring this hNFT.
-							<a className='bidLink' href={``} target="_blank">I want to bid</a>
+							<span>is sponsoring this hNFT. </span>
+							<a className='bidLink' href={``} target="_blank">Bid on this ad space</a>
 						</span>
 					</div>
 					<div className='adSection'>
-						<div className='adSectionArrow'></div>
+						<img className='adBubble' src={bubble}></img>
 						<div className='adContent'>
 							<div className='adDescription'>
-								<span className='descriptionText'>{ad?.content ?? ad?.description ?? 'View Ads. Get Paid.'}</span>
+								<span className='descriptionText'>{ad?.content ?? 'View Ads. Get Paid.'}</span>
 								{tags?.length > 0 && <span className='tags'>
 									{tags.map((tag: string, index: number) => <span className='tag' key={index}>#{tag}</span>)}
 								</span>}
 							</div>
-
-							{ad?.poster && <img
-								src={ad?.poster}
-								referrerPolicy='no-referrer'
-								className='adMediaImg'
-							/>}
+							{!!ad?.poster && <>
+								<img
+									src={ad?.poster}
+									referrerPolicy='no-referrer'
+									className='adMediaImg'
+								/>
+							</>}
 							{!ad?.poster && <div className='posterPlaceHolder'>Your poster goes here</div>}
-
 						</div>
 					</div>
+
+					<div className='divider'></div>
 
 					{<div className='claimSection'>
 						<div className='infoText'>{
@@ -74,14 +78,23 @@ const AdvertisementPreview: React.FC<{
 							</div>
 						</div>
 
-						{<>
+						{claimed && <>
+							<div className='btnContainer'>
+								<div className='actionBtnBig left' onClick={async () => {
+									window.open(`https://twitter.com/intent/tweet?text=Hundreds of Celebrity NFT Powers awaits you to FREE claim! Install and GemHunt on Twitter HERE ❤️ @ParamiProtocol&url=https://chrome.google.com/webstore/detail/parami-hyperlink-nft-exte/gilmlbeecofjmogfkaocnjmbiblmifad`);
+								}}>Share</div>
+								<div className='actionBtnBig right' onClick={() => window.open(`/swap/${ad.nftId}`)}>Support this influencer</div>
+							</div>
+						</>}
+
+						{!claimed && <>
 							{ad?.instructions?.length > 0 && <>
 								<div className='instructionSection'>
 									<div className='instructionTitle'>Follow the tips below if you are interested</div>
 									{ad.instructions.map((instruction: any, index: number) => {
 										return (
 											<div className='instruction' onClick={() => {
-												// !!instruction.link && window.open(`https://weekly.parami.io?redirect=${instruction.link}&nftId=${ad.nftId}&did=${userDid}&ad=${ad.adId}&tag=${instruction.tag}&score=${instruction.score}`);
+												!!instruction.link && window.open(`https://weekly.parami.io?redirect=${instruction.link}&nftId=${ad.nftId}`);
 												setClaimText('Claim');
 											}} key={index}>
 												<span className='instructionText'>{instruction.text}</span>
@@ -94,11 +107,13 @@ const AdvertisementPreview: React.FC<{
 							</>}
 
 							<div className='btnContainer'>
-								<div className='actionBtnBig' onClick={() => null}>{claimText}</div>
+								<div className='actionBtnBig' onClick={() => {
+									setClaimed(true);
+								}}>{claimText}</div>
 							</div>
 						</>}
 					</div>}
-				</>}
+				</>
 
 			</div>
 		</>
