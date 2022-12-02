9fe62647277cca63c1175b1ac1554004dec18cbd
kaikang.eth <cctv2206@outlook.com>
2022-12-02 17:55:43 +0800

    MOD: clock in claim page

---
+++ src/pages/ClockInClaim/ClockInClaim.tsx
@@ -0,0 +1,84 @@
+import BigModal from '@/components/ParamiModal/BigModal';
+import SecurityModal from '@/components/ParamiModal/SecurityModal';
+import { POST_MESSAGE_PREFIX } from '@/config/constant';
+import { UserClockIn } from '@/services/parami/ClockIn.service';
+import { notification, Spin } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { useIntl, useModel, useParams } from 'umi';
+import style from './ClockInClaim.less';
+
+export interface ClockInClaimProps { }
+
+function ClockInClaim({ }: ClockInClaimProps) {
+    const [secModal, setSecModal] = useState<boolean>(false);
+    const [passphrase, setPassphrase] = useState<string>('');
+    const { wallet } = useModel('currentUser');
+    const apiWs = useModel('apiWs');
+    const intl = useIntl();
+    const {
+        adId,
+        nftId
+    } = useParams() as { adId: string, nftId: string };
+
+    const close = () => {
+        window.opener.postMessage(`${POST_MESSAGE_PREFIX.AD_CLAIMED}:${adId}`, '*');
+        window.close();
+    }
+
+    useEffect(() => {
+        if (nftId && apiWs && wallet) {
+            setSecModal(true);
+        }
+    }, [nftId, apiWs, wallet]);
+
+    const claim = async (preTx?: boolean, account?: string) => {
+        if (!wallet?.keystore) {
+            notification.error({
+                key: 'accessDenied',
+                message: intl.formatMessage({
+                    id: 'error.accessDenied',
+                }),
+                duration: null,
+            });
+            return;
+        }
+
+        try {
+            await UserClockIn(nftId, passphrase, wallet.keystore, preTx, account);
+            close();
+        } catch (e) {
+            console.log(e);
+            notification.error({
+                message: 'Claim Ad Token Error',
+                description: `${e}`,
+            });
+        }
+    }
+
+    return <>
+        <BigModal
+            visable
+            title="Claim your token"
+            content={
+                <div className={style.container}>
+                    <Spin></Spin>
+                </div>
+            }
+            footer={null}
+            close={() => {
+                window.close();
+            }}
+        ></BigModal>
+
+        <SecurityModal
+            visable={secModal}
+            setVisable={setSecModal}
+            passphrase={passphrase}
+            setPassphrase={setPassphrase}
+            directSubmit={true}
+            func={claim}
+        ></SecurityModal>
+    </>;
+};
+
+export default ClockInClaim;
