cd287bd18beaef53111621e46f83cee3c9f5b6c1
kaikang.eth <cctv2206@outlook.com>
2023-02-16 16:13:40 +0800

    MOD: bid with fraction when nft minted

--- src/pages/QuickBid/QuickBid.tsx
+++ src/pages/QuickBid/QuickBid.tsx
@@ -1,7 +1,7 @@
 import BigModal from '@/components/ParamiModal/BigModal';
 import SecurityModal from '@/components/ParamiModal/SecurityModal';
 import { POST_MESSAGE_PREFIX } from '@/config/constant';
-import { UserBidSlotWithAd3 } from '@/services/parami/Advertisement';
+import { UserBidSlot, UserBidSlotWithAd3 } from '@/services/parami/Advertisement';
 import { parseAmount } from '@/utils/common';
 import { notification, Spin } from 'antd';
 import React, { useEffect, useState } from 'react';
@@ -16,12 +16,16 @@ function QuickBid({ }: QuickBidProps) {
     const { wallet } = useModel('currentUser');
     const apiWs = useModel('apiWs');
     const { nftId, amount } = useParams() as { nftId: string, amount: string };
+    const [nftMinted, setNftMinted] = useState<boolean>();
 
     const handleBidAd = async (preTx?: boolean, account?: string) => {
         try {
-            // todo: default adId
+            // todo: default adId and never expire
             const price = Number(amount) > 0 ? amount : parseAmount('1');
-            const info: any = await UserBidSlotWithAd3('0x0ec3c7ad65614705ca4b5c2cfdc375b06fb40b41e8490b9459087dfc2842671f', nftId, price, passphrase, wallet?.keystore, preTx, account);
+
+            const info: any = nftMinted
+                ? await UserBidSlot('0x9ed392c64a7a5b7e2316633d1b57631adbea4f8349310c341302487f41382c33', nftId, price, passphrase, wallet?.keystore, preTx, account)
+                : await UserBidSlotWithAd3('0x9ed392c64a7a5b7e2316633d1b57631adbea4f8349310c341302487f41382c33', nftId, price, passphrase, wallet?.keystore, preTx, account);
 
             if (preTx && account) {
                 return info;
@@ -45,6 +49,12 @@ function QuickBid({ }: QuickBidProps) {
     useEffect(() => {
         if (apiWs) {
             setSecModal(true);
+
+            (async () => {
+                const nftMetaRes = await apiWs.query.nft.metadata(nftId);
+                const nft = nftMetaRes.toHuman() as any;
+                setNftMinted(nft.minted);
+            })();
         }
     }, [apiWs])
 
